dcefb88eecdc83abcd1f10abe162c31c3af780a5
Martha <marthanakibirango74@gmail.com>
2025-10-17 18:42:26 +0300

    added

--- Tech_assement/MARTHA/views.py
+++ Tech_assement/MARTHA/views.py
@@ -7,42 +7,25 @@ from django.db.models import Q
 
 CustomUser = get_user_model()
 
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from MARTHA.models import CustomUser  
 
 def loginPage(request):
     if request.method == 'POST':
-        identifier = request.POST.get('username') 
+        identifier = request.POST.get('username')
         password = request.POST.get('password')
 
         if not identifier or not password:
             messages.error(request, "All fields are required.")
-            return render(request, 'login.html') 
-
-       
-        try:
-            user_obj = CustomUser.objects.get(
-                Q(username=identifier) |
-                Q(email=identifier) |
-                Q(phone_number=identifier)
-            )
-            
-            
-            django_username = user_obj.username 
-            
-            
-            user = authenticate(request, username=django_username, password=password)
-
-        except CustomUser.DoesNotExist:
-            user = None
-        except Exception:
-            user = None
-        
-        if user:
-            login(request, user)
-            messages.success(request, "Congratulations! You’ve successfully logged in.")
-            return redirect('loginPage') 
         else:
-
-            messages.error(request, "Invalid credentials (user not found or bad password).") 
+            user = authenticate(request, username=identifier, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, "Successfully logged in.")
+                return redirect('dashboard') 
+            else:
+                messages.error(request, "Invalid credentials.")
 
     return render(request, 'login.html')
 
